958ca9e016edbdc39d34014aaae5c77de0908021
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2021-12-26 11:05:16 +0530

    Time: 304 ms (20.07%), Space: 66.3 MB (24.75%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
-    static bool compare(vector<int>& a, vector<int>& b) 
-    {
+    struct comp{
+        bool operator()(vector<int>& a, vector<int>& b) 
+        {
             return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
-    }
+        }
+    };
     vector<vector<int>> kClosest(vector<vector<int>>& v, int K) {
-        
-        sort(v.begin(),v.end(),compare);
-        return vector<vector<int>>(v.begin(), v.begin() + K);
+        priority_queue<vector<int>,vector<vector<int>>,comp>pq;
+        for(auto x:v){
+            pq.push(x);
+            if(pq.size()>K)pq.pop();
+        }
+        vector<vector<int>>vec;
+        while(!pq.empty()){
+            vec.push_back(pq.top());
+            pq.pop();
+        }
+        return vec;
     }
 };
