318b96845080efa06d0687ba912c74b4850bcc52
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2022-01-05 11:51:20 +0530

    Time: 223 ms (21.51%), Space: 57.7 MB (53.18%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    vector<vector<string>>vec;
-    int n;
-    
     vector<vector<string>> partition(string s) {
+        vector<vector<string>>vec;
         vector<string>v;
-        n=s.length();
-        if(n==0)return vec;
-        dfs(s,0,v);
+        if(s.length()==0)return vec;
+        dfs(s,0,s.length(),vec,v);
         return vec;
     }
     
-    void dfs(string s,int idx,vector<string>&v)
+    void dfs(string s,int idx,int n,vector<vector<string>>&vec,vector<string>&v)
     {
-        if(idx==n)vec.push_back(v);
-        else{
+        if(idx==n){vec.push_back(v);return;}
         for(int i=idx;i<n;i++)
+        {
+            if(palindrome(idx,i,s))
             {
-                if(palindrome(idx,i,s))
-                {
-                    v.push_back(s.substr(idx,i-idx+1));
-                    dfs(s,i+1,v);
-                    v.pop_back();
-                }
+                v.push_back(s.substr(idx,i-idx+1));
+                dfs(s,i+1,s.length(),vec,v);
+                v.pop_back();
             }
         }
     }
