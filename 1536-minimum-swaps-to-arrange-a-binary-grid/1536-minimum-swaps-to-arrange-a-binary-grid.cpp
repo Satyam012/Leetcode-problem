8a2fbb737f6715e2ba638e1c4ee0cd2d79e7c4f7
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2022-01-25 15:42:49 +0530

    Time: 95 ms (30.60%), Space: 25.6 MB (55.97%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -8,24 +8,32 @@ public:
     }
     int minSwaps(vector<vector<int>>& grid) {
         int ans=0,m=grid.size(),n=grid[0].size();
+        vector<int>count(m);
+        
         for(int i=0;i<m;i++){
-            bool flag=valid(i+1,grid[i]);
+            int cnt=0,j=n-1;
+            while(j>=0 && grid[i][j]==0){
+                cnt++;
+                j--;
+            }
+            count[i]=cnt;
+        }
+        
+        for(int i=0;i<m;i++){
+            if(count[i]>=n-i-1)continue;
             
-            if(flag==false){
-                int j=i+1;
-                while(j<m){
-                    if(valid(i+1,grid[j]))break;
-                    j++;
-                }
-                if(j==m)return -1;
-                else{
-                    ans+=j-i;
-                    while(j>i){
-                        swap(grid[j],grid[j-1]);
-                        j--;
-                    }
+            int j=i+1;
+            while(j<m &&count[j]<n-i-1)j++;
+                
+            if(j==m)return -1;
+            else{
+                ans+=j-i;
+                while(j>i){
+                    swap(count[j],count[j-1]);
+                    j--;
                 }
             }
+            
         }
         return ans;
     }
