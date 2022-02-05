fc0dcce66e52ed8f508b65f4d32b5b5d4db830b7
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2022-02-05 17:05:59 +0530

    Time: 56 ms (10.55%), Space: 16.3 MB (15.03%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<vector<int>>vec;
+    int n;
+    vector<int>cand;
     vector<vector<int>> combinationSum2(vector<int>&can, int s) {
         sort(can.begin(),can.end());
         vector<int>v;
-        test(0,can.size(),can,s,v);
+        n=can.size();
+        cand=can;
+        test(0,s,v);
         return vec;
     }
-    void test(int i,int n,vector<int>&can,int s,vector<int>v){
+    void test(int i,int s,vector<int>v){
         if(s==0){
             vec.push_back(v);
             return;
         }
         if(s<0||i>=n)return;
         for(int j=i;j<n;j++){
-            if(j>i && can[j]==can[j-1]) continue;
-            v.push_back(can[j]);
-            test(j+1,n,can,s-can[j],v);
+            if(j>i && cand[j]==cand[j-1]) continue;
+            v.push_back(cand[j]);
+            test(j+1,s-cand[j],v);
             v.pop_back();
         }
     }    
