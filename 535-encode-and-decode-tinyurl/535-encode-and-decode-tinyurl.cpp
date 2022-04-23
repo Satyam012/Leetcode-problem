1e10e3faf35a065aac295ac6d3cb92e228558467
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2022-04-24 02:16:17 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (29.41%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,17 +1,35 @@
 class Solution {
+private:
+    unordered_map<string,string> db;
+    string base64Strs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    string base62Encoding(int unix){
+        string tinyURL;
+        while(unix > 0){
+            tinyURL += base64Strs[unix % 62];
+            unix /= 62;
+        }
+        return tinyURL;
+    }
+    
 public:
 
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        time_t id = time(NULL);
+        //http://tinyurl.com/4e9iAk
+        string shortenURL = base62Encoding(id);
+        string tinyURL = "http://tinyurl.com/"+ shortenURL;
+        db[shortenURL] = longUrl;
+        return tinyURL;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        //"https://leetcode.com/problems/design-tinyurl"
+        //http://tinyurl.com/bNiRXb
+
+        string code;
+        for(int i = 19;i<shortUrl.size();i++) code += shortUrl[i];
+        return db[code];
     }
 };
-
-// Your Solution object will be instantiated and called as such:
-// Solution solution;
-// solution.decode(solution.encode(url));
