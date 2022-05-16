927136d77a6c4f5d1edd379015e4b3c2b0d41af1
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2022-05-17 00:44:08 +0530

    Time: 327 ms (88.35%), Space: 102.1 MB (79.24%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_set<string>st;
+        unordered_map<string,int>indegree;
+        unordered_map<string,vector<string>>graph;
+        
+        for(auto &s : supplies)st.insert(s);
+        for(auto &x : recipes)indegree[x] = 0;
+        
+        for(int i=0;i<recipes.size();i++){
+            for(int j=0;j<ingredients[i].size();j++){
+                if(st.find(ingredients[i][j])==st.end()){
+                    indegree[recipes[i]]++;
+                    graph[ingredients[i][j]].push_back(recipes[i]);
+                }
+            }
+        }
+        queue<string>q;
+        for(auto &x:indegree){
+            if(x.second==0)q.push(x.first);
+        }
+        vector<string>res;
+        while(!q.empty()){
+            string top=q.front();
+            q.pop();
+            res.push_back(top);
+            for(auto &x:graph[top]){
+                indegree[x]--;
+                if(indegree[x]==0)q.push(x);
+            }
+        }
+        return res;
+    }
+};
